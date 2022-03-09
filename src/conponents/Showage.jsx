70caebf88336e15660473e65b1da657e8d114c7c
Hungtv2951 <hungtv290501@gmail.com>
2022-03-09 09:35:04 +0700

    react

---
+++ src/conponents/Showage.jsx
@@ -0,0 +1,2 @@
+const ShowAge = props => <p>Tuổi của bạn là: {props.age}</p>
+export default ShowAge;
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import ShowAge from './conponents/Showage'
+const person = {
+  name: "Hùng",
+  age: 200,
+  status: true,
+  children: [
+    { id: "1", name: "Dương" },
+    { id: "2", name: "Nghĩa" },
+  ]
+}
+// sử dụng hàm
+const showAge = (age) => <p>Tuổi của bạn là: {age}</p>
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render(<div>
+  <p>Tên: {person.name}</p>
+  <p>Tuổi: {person.age}</p>
+  <p>Con trai: {person.children.map(item => <li>
+    {item.name}
+  </li>)}</p>
+  <p>function: {showAge(person.age)}</p>
+  <p>Conponent: <ShowAge age={person.age} /></p>
+</div>, document.getElementById('root')
 )
